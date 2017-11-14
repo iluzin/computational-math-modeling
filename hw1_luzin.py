9c68ca54cbfe3e6bad2634323634ea351caba306
Игорь <isluzin@mail.ru>
2017-11-14 10:29:42 +0300

    task1

---
+++ hw1_luzin.py
@@ -0,0 +1,145 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+from mpl_toolkits.mplot3d.art3d import Poly3DCollection
+
+EPS = 1e-6
+
+def area(a, b, c):
+    return np.linalg.norm(np.cross(b - a, c - a)) / 2
+
+def intersect(a, b, c, d):
+    res = all(max(min(i, j), min(k, l)) - min(max(i, j), max(k, l)) < EPS for i, j, k, l in zip(a, b, c, d))
+    res &= np.dot(np.cross(b - a, c - a), np.cross(b - a, d - a)) < EPS
+    res &= np.dot(np.cross(d - c, a - c), np.cross(d - c, b - c)) < EPS
+    return res
+
+def process(a, b, c, p, q):
+    coef = np.cross(b - a, c - a)
+    intercept = -coef.dot(a)
+    res = False
+    if np.abs(coef.dot(p - q)) < EPS:
+        if np.abs(coef.dot(p) + intercept) < EPS:
+            res |= area(a, b, p) + area(b, c, p) + area(c, a, p) - area(a, b, c) < EPS
+            res |= intersect(a, b, p, q) or intersect(b, c, p, q) or intersect(c, a, p, q)
+    else:
+        mu = (coef.dot(p) + intercept) / coef.dot(p - q)
+        if -EPS < mu and mu < 1 + EPS:
+            m = p + mu * (q - p)
+            res |= area(a, b, m) + area(b, c, m) + area(c, a, m) - area(a, b, c) < EPS
+    return res
+
+if __name__ == '__main__':
+    trianglePairs = []
+    
+    # 1
+    p1 = [0, 0, 0]
+    p2 = [0, 5, 0]
+    p3 = [6, 5, 0]
+    tr1 = [p1, p2, p3]
+    q1 = [1, 4, 0]
+    q2 = [2, 4, 0]
+    q3 = [2, 3, 0]
+    tr2 = [q1, q2, q3]
+    trianglePairs.append((tr1, tr2))
+    
+    # 2
+    p1 = [-1, 0, 0]
+    p2 = [0, -1, 0]
+    p3 = [0, 0, 0]
+    tr1 = [p1, p2, p3]
+    q1 = [0, 0, 0]
+    q2 = [0, 3, 0]
+    q3 = [5, 0, 0]
+    tr2 = [q1, q2, q3]
+    trianglePairs.append((tr1, tr2))
+    
+    # 3
+    p1 = [-1, 0, 0]
+    p2 = [0, 2, 0]
+    p3 = [0, 0, 0]
+    tr1 = [p1, p2, p3]
+    q1 = [0, 0, 0]
+    q2 = [5, 0, 0]
+    q3 = [0, 4, 0]
+    tr2 = [q1, q2, q3]
+    trianglePairs.append((tr1, tr2))
+    
+    # 4
+    p1 = [0, 0, 0]
+    p2 = [0, 2, 0]
+    p3 = [1, 0, 0]
+    tr1 = [p1, p2, p3]
+    q1 = [0, -1, 0]
+    q2 = [0, 3, 0]
+    q3 = [7, -1, 0]
+    tr2 = [q1, q2, q3]
+    trianglePairs.append((tr1, tr2))
+    
+    # 5
+    p1 = [0, 0, 0]
+    p2 = [0, 4, 0]
+    p3 = [4, 0, 0]
+    tr1 = [p1, p2, p3]
+    q1 = [1, 2, 0]
+    q2 = [1, 1, -3]
+    q3 = [0.5, 2, -2]
+    tr2 = [q1, q2, q3]
+    trianglePairs.append((tr1, tr2))
+    
+    # 6
+    p1 = [0, 0, 0]
+    p2 = [0, 4, 0]
+    p3 = [4, 0, 0]
+    tr1 = [p1, p2, p3]
+    q1 = [4, 0, 0]
+    q2 = [1, 1, -3]
+    q3 = [0.5, 2, -2]
+    tr2 = [q1, q2, q3]
+    trianglePairs.append((tr1, tr2))
+    
+    # 7
+    p1 = [0, 0, 0]
+    p2 = [0, 4, 0]
+    p3 = [4, 0, 0]
+    tr1 = [p1, p2, p3]
+    q1 = [1, 2, 0]
+    q2 = [2, 1, 0]
+    q3 = [0.5, 2, -2]
+    tr2 = [q1, q2, q3]
+    trianglePairs.append((tr1, tr2))
+    
+    # 8
+    p1 = [0, 0, 0]
+    p2 = [0, 4, 0]
+    p3 = [4, 0, 0]
+    tr1 = [p1, p2, p3]
+    q1 = [-1, 2, 2]
+    q2 = [0, 2, 2]
+    q3 = [0, 0, -2]
+    tr2 = [q1, q2, q3]
+    trianglePairs.append((tr1, tr2))
+    
+    # 9
+    p1 = [0, 0, 0]
+    p2 = [0, 4, 0]
+    p3 = [5, 0, 0]
+    tr1 = [p1, p2, p3]
+    q1 = [1, 1, 2]
+    q2 = [5, 6, -2]
+    q3 = [3, -4, -1]
+    tr2 = [q1, q2, q3]
+    trianglePairs.append((tr1, tr2))
+    
+    for item in trianglePairs:
+        tr1, tr2 = np.asarray(item)
+        p1, p2, p3 = tr1
+        q1, q2, q3 = tr2
+        print process(p1, p2, p3, q1, q2) or process(p1, p2, p3, q2, q3) or process(p1, p2, p3, q3, q1) or process(q1, q2, q3, p1, p2) or process(q1, q2, q3, p2, p3) or process(q1, q2, q3, p3, p1)
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        ax.add_collection3d(Poly3DCollection(item))
+        ax.set_xlim(-5, 5)
+        ax.set_ylim(-5, 5)
+        ax.set_zlim(-5, 5)
+        plt.show()
--- hw3_luzin.py
+++ hw3_luzin.py
@@ -1,166 +1,185 @@
 from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
-from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT as NavigationToolbar
 import matplotlib.pyplot as plt
-from PyQt5.QtCore import Qt
-from PyQt5 import QtGui, QtWidgets
+from PyQt5 import QtCore, QtGui, QtWidgets
 import sys
 
-class Object:
+class Application(QtWidgets.QApplication):
+    def exec_(self):
+        window = Window()
+        window.setWindowTitle(__file__)
+        window.show()
+        return QtWidgets.QApplication.exec_()
+
+class Circle(object):
     def __init__(self, x=0, y=0, radius=1, color=None):
         self.x = x
         self.y = y
         self.radius = radius
         self.color = color
+    
+    def pos(self):
+        return self.x, self.y
+    
+    def x(self, x=None):
+        if not x is None:
+            self.x = x
+        return self.x
+    
+    def y(self, y=None):
+        if not y is None:
+            self.y = y
+        return self.y
 
-def onmove(event):
-    if event.xdata is None:
-        xline.setText('')
-    else:
-        xline.setText(str(round(event.xdata, 1)))
-    if event.ydata is None:
-        yline.setText('')
-    else:
-        yline.setText(str(round(event.ydata, 1)))
+class Window(QtWidgets.QWidget):
+    def __init__(self, *args, **kwargs):
+        QtWidgets.QWidget.__init__(self, *args, **kwargs)
+        hbox = QtWidgets.QHBoxLayout()
+        self.setLayout(hbox)
+        vbox_left = QtWidgets.QVBoxLayout()
+        vbox_right = QtWidgets.QVBoxLayout()
+        hbox.addLayout(vbox_left)
+        hbox.addLayout(vbox_right)
+        figure = plt.figure()
+        self.canvas = FigureCanvas(figure)
+        vbox_left.addWidget(self.canvas)
+        hbox_zoom = QtWidgets.QHBoxLayout()
+        vbox_left.addLayout(hbox_zoom)
+        button_plus = QtWidgets.QPushButton('+')
+        button_plus.clicked.connect(self.plusButtonClickedEvent)
+        button_minus = QtWidgets.QPushButton('-')
+        button_minus.clicked.connect(self.minusButtonClickedEvent)
+        hbox_zoom.addWidget(button_plus)
+        hbox_zoom.addWidget(button_minus)
+        hbox_open = QtWidgets.QHBoxLayout()
+        hbox_save = QtWidgets.QHBoxLayout()
+        vbox_left.addLayout(hbox_save)
+        vbox_left.addLayout(hbox_open)
+        openlabel = QtWidgets.QLabel("Open:")
+        self.openfilepath = QtWidgets.QLineEdit()
+        self.openfilepath.setDisabled(True)
+        button_open = QtWidgets.QPushButton('Browse...')
+        button_open.clicked.connect(self.openFileEvent)
+        hbox_open.addWidget(openlabel)
+        hbox_open.addWidget(self.openfilepath)
+        hbox_open.addWidget(button_open)
+        savelabel = QtWidgets.QLabel("Save:")
+        self.savefilepath = QtWidgets.QLineEdit()
+        self.savefilepath.setDisabled(True)
+        button_save = QtWidgets.QPushButton('Browse...')
+        button_save.clicked.connect(self.saveFileEvent)
+        hbox_save.addWidget(savelabel)
+        hbox_save.addWidget(self.savefilepath)
+        hbox_save.addWidget(button_save)
+        tabs = QtWidgets.QTabWidget()
+        hbox.addWidget(tabs)
+        tab_edit = QtWidgets.QWidget()
+        tabs.addTab(tab_edit, 'Edit')
+        vbox_edit = QtWidgets.QVBoxLayout()
+        tab_edit.setLayout(vbox_edit)
+        tab_model = QtWidgets.QWidget()
+        tabs.addTab(tab_model, 'Model')
+        hbox_pos = QtWidgets.QHBoxLayout()
+        vbox_edit.addLayout(hbox_pos)
+        xlabel = QtWidgets.QLabel("X:")
+        ylabel = QtWidgets.QLabel("Y:")
+        self.xline = QtWidgets.QLineEdit()
+        self.xline.setDisabled(True)
+        self.yline = QtWidgets.QLineEdit()
+        self.yline.setDisabled(True)
+        self.canvas.mpl_connect('motion_notify_event', self.mouseMoveEvent)
+        self.canvas.mpl_connect('button_press_event', self.mouseClickEvent)
+        hbox_pos.addWidget(xlabel)
+        hbox_pos.addWidget(self.xline)
+        hbox_pos.addWidget(ylabel)
+        hbox_pos.addWidget(self.yline)
+        hbox_color = QtWidgets.QHBoxLayout()
+        vbox_edit.addLayout(hbox_color)
+        self.colorbox = QtWidgets.QComboBox()
+        self.colorbox.addItem('Red')
+        self.colorbox.addItem('Green')
+        self.colorbox.addItem('Blue')
+        colorlabel = QtWidgets.QLabel('Color:')
+        hbox_color.addWidget(colorlabel)
+        hbox_color.addWidget(self.colorbox)
+        hbox_size = QtWidgets.QHBoxLayout()
+        vbox_edit.addLayout(hbox_size)
+        sizelabel = QtWidgets.QLabel('Size:')
+        self.sizeline = QtWidgets.QLineEdit()
+        hbox_size.addWidget(sizelabel)
+        hbox_size.addWidget(self.sizeline)
+        self.slider = QtWidgets.QSlider(QtCore.Qt.Horizontal)
+        vbox_edit.addWidget(self.slider)
+        self.slider.setMaximum(100)
+        self.slider.setMinimum(1)
+        self.slider.sliderMoved.connect(self.sliderChangeEvent)
+        self.slider.sliderPressed.connect(self.sliderChangeEvent)
+        self.sizeline.setText(str(self.slider.value()))
+        self.sizeline.textEdited.connect(self.sizeChangedEvent)
+        vbox_model = QtWidgets.QVBoxLayout()
+        tab_model.setLayout(vbox_model)
+        button_scipy = QtWidgets.QRadioButton('scipy')
+        vbox_model.addWidget(button_scipy)
+        button_verlet = QtWidgets.QRadioButton('verlet')
+        vbox_model.addWidget(button_verlet)
+        button_verlet_threading = QtWidgets.QRadioButton('verlet-threading')
+        vbox_model.addWidget(button_verlet_threading)
+        button_verlet_multiprocessing = QtWidgets.QRadioButton('verlet-multiprocessing')
+        vbox_model.addWidget(button_verlet_multiprocessing)
+        button_verlet_cython = QtWidgets.QRadioButton('verlet-cython')
+        vbox_model.addWidget(button_verlet_cython)
+        button_verlet_opencl = QtWidgets.QRadioButton('verlet-opencl')
+        vbox_model.addWidget(button_verlet_opencl)
+        plt.axis('on')
+        plt.xlim(-100, 100)
+        plt.ylim(-100, 100)
+        self.circles = []
+    
+    def minusButtonClickedEvent(self):
+        plt.xlim(plt.xlim()[0] * 1.5, plt.xlim()[1] * 1.5)
+        plt.ylim(plt.ylim()[0] * 1.5, plt.ylim()[1] * 1.5)
+        self.canvas.draw()
+    
+    def mouseClickEvent(self, event):
+        if not event.xdata is None and not event.ydata is None:
+            circle = Circle(event.xdata, event.ydata, self.slider.value(), self.colorbox.currentText())
+            plt.subplot().add_patch(plt.Circle(circle.pos(), radius=circle.radius, color=circle.color))
+            self.canvas.draw()
+            self.circles.append(circle)
+    
+    def mouseMoveEvent(self, event):
+        if event.xdata is None:
+            self.xline.setText(str())
+        else:
+            self.xline.setText(str(round(event.xdata, 1)))
+        if event.ydata is None:
+            self.yline.setText(str())
+        else:
+            self.yline.setText(str(round(event.ydata, 1)))
+    
+    def openFileEvent(self):
+        path = QtWidgets.QFileDialog.getOpenFileName(self, filter='*.xml')
+        self.openfilepath.setText(path[0])
+    
+    def plusButtonClickedEvent(self):
+        plt.xlim(plt.xlim()[0] / 1.5, plt.xlim()[1] / 1.5)
+        plt.ylim(plt.ylim()[0] / 1.5, plt.ylim()[1] / 1.5)
+        self.canvas.draw()
+    
+    def saveFileEvent(self):
+        path = QtWidgets.QFileDialog.getSaveFileName(self, filter='*.xml')
+        self.savefilepath.setText(path[0])
+    
+    def sizeChangedEvent(self):
+        size = 1
+        try:
+            size = max(1, min(int(self.sizeline.text()), 100))
+        except:
+            pass
+        self.slider.setValue(size)
+    
+    def sliderChangeEvent(self):
+        self.sizeline.setText(str(self.slider.value()))
 
-def onpress(event):
-    if not event.xdata is None and not event.ydata is None:
-        obj = Object(event.xdata, event.ydata, slider.value(), colorbox.currentText())
-        plt.subplot().add_patch(plt.Circle((obj.x, obj.y), radius=obj.radius, color=obj.color))
-        canvas.draw()
-        objects.append(obj)
-
-def minus():
-    plt.xlim(plt.xlim()[0] * 1.5, plt.xlim()[1] * 1.5)
-    plt.ylim(plt.ylim()[0] * 1.5, plt.ylim()[1] * 1.5)
-    canvas.draw()
-
-def plus():
-    plt.xlim(plt.xlim()[0] / 1.5, plt.xlim()[1] / 1.5)
-    plt.ylim(plt.ylim()[0] / 1.5, plt.ylim()[1] / 1.5)
-    canvas.draw()
-
-def openfiledialog():
-    path = QtWidgets.QFileDialog.getOpenFileName(w, filter='*.xml')
-    openfilepath.setText(path[0])
-
-def savefiledialog():
-    path = QtWidgets.QFileDialog.getSaveFileName(w, filter='*.xml')
-    savefilepath.setText(path[0])
-
-def changed():
-    sizeline.setText(str(slider.value()))
-
-def edited():
-    size = 1
-    try:
-        size = max(1, min(int(sizeline.text()), 100))
-    except:
-        pass
-    slider.setValue(size)
-
-app = QtWidgets.QApplication(sys.argv)
-w = QtWidgets.QWidget()
-w.resize(800, 600)
-w.setWindowTitle('Task 3')
-hbox = QtWidgets.QHBoxLayout()
-w.setLayout(hbox)
-vbox_left = QtWidgets.QVBoxLayout()
-vbox_right = QtWidgets.QVBoxLayout()
-hbox.addLayout(vbox_left)
-hbox.addLayout(vbox_right)
-figure = plt.figure()
-canvas = FigureCanvas(figure)
-vbox_left.addWidget(canvas)
-hbox_zoom = QtWidgets.QHBoxLayout()
-vbox_left.addLayout(hbox_zoom)
-button_plus = QtWidgets.QPushButton('+')
-button_plus.clicked.connect(plus)
-button_minus = QtWidgets.QPushButton('-')
-button_minus.clicked.connect(minus)
-hbox_zoom.addWidget(button_plus)
-hbox_zoom.addWidget(button_minus)
-hbox_open = QtWidgets.QHBoxLayout()
-hbox_save = QtWidgets.QHBoxLayout()
-vbox_left.addLayout(hbox_save)
-vbox_left.addLayout(hbox_open)
-openlabel = QtWidgets.QLabel("Open:")
-openfilepath = QtWidgets.QLineEdit()
-openfilepath.setDisabled(True)
-button_open = QtWidgets.QPushButton('Browse...')
-button_open.clicked.connect(openfiledialog)
-hbox_open.addWidget(openlabel)
-hbox_open.addWidget(openfilepath)
-hbox_open.addWidget(button_open)
-savelabel = QtWidgets.QLabel("Save:")
-savefilepath = QtWidgets.QLineEdit()
-savefilepath.setDisabled(True)
-button_save = QtWidgets.QPushButton('Browse...')
-button_save.clicked.connect(savefiledialog)
-hbox_save.addWidget(savelabel)
-hbox_save.addWidget(savefilepath)
-hbox_save.addWidget(button_save)
-tabs = QtWidgets.QTabWidget()
-hbox.addWidget(tabs)
-tab_edit = QtWidgets.QWidget()
-tabs.addTab(tab_edit, 'Edit')
-vbox_edit = QtWidgets.QVBoxLayout()
-tab_edit.setLayout(vbox_edit)
-tab_model = QtWidgets.QWidget()
-tabs.addTab(tab_model, 'Model')
-hbox_pos = QtWidgets.QHBoxLayout()
-vbox_edit.addLayout(hbox_pos)
-xlabel = QtWidgets.QLabel("X:")
-ylabel = QtWidgets.QLabel("Y:")
-xline = QtWidgets.QLineEdit()
-xline.setDisabled(True)
-yline = QtWidgets.QLineEdit()
-yline.setDisabled(True)
-canvas.mpl_connect('motion_notify_event', onmove)
-canvas.mpl_connect('button_press_event', onpress)
-hbox_pos.addWidget(xlabel)
-hbox_pos.addWidget(xline)
-hbox_pos.addWidget(ylabel)
-hbox_pos.addWidget(yline)
-hbox_color = QtWidgets.QHBoxLayout()
-vbox_edit.addLayout(hbox_color)
-colorbox = QtWidgets.QComboBox()
-colorbox.addItem('Red')
-colorbox.addItem('Green')
-colorbox.addItem('Blue')
-colorlabel = QtWidgets.QLabel('Color:')
-hbox_color.addWidget(colorlabel)
-hbox_color.addWidget(colorbox)
-hbox_size = QtWidgets.QHBoxLayout()
-vbox_edit.addLayout(hbox_size)
-sizelabel = QtWidgets.QLabel('Size:')
-sizeline = QtWidgets.QLineEdit()
-hbox_size.addWidget(sizelabel)
-hbox_size.addWidget(sizeline)
-slider = QtWidgets.QSlider(Qt.Horizontal)
-vbox_edit.addWidget(slider)
-slider.setMaximum(100)
-slider.setMinimum(1)
-slider.sliderMoved.connect(changed)
-slider.sliderPressed.connect(changed)
-sizeline.setText(str(slider.value()))
-sizeline.textEdited.connect(edited)
-vbox_model = QtWidgets.QVBoxLayout()
-tab_model.setLayout(vbox_model)
-button_scipy = QtWidgets.QRadioButton('scipy')
-vbox_model.addWidget(button_scipy)
-button_verlet = QtWidgets.QRadioButton('verlet')
-vbox_model.addWidget(button_verlet)
-button_verlet_threading = QtWidgets.QRadioButton('verlet-threading')
-vbox_model.addWidget(button_verlet_threading)
-button_verlet_multiprocessing = QtWidgets.QRadioButton('verlet-multiprocessing')
-vbox_model.addWidget(button_verlet_multiprocessing)
-button_verlet_cython = QtWidgets.QRadioButton('verlet-cython')
-vbox_model.addWidget(button_verlet_cython)
-button_verlet_opencl = QtWidgets.QRadioButton('verlet-opencl')
-vbox_model.addWidget(button_verlet_opencl)
-plt.axis('on')
-plt.xlim(-100, 100)
-plt.ylim(-100, 100)
-objects = []
-w.show()
-sys.exit(app.exec_())
+if __name__ == '__main__':
+    app = Application(sys.argv)
+    sys.exit(app.exec_())
